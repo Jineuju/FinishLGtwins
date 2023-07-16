2669312b1d56301d4e036fbcc54d21465c7cc131
Jineuju <juj4628@gmail.com>
2023-07-16 23:05:33 +0900

    진의주 포트폴리오 마무리

---
+++ script/news.js
@@ -0,0 +1,129 @@
+// 1번째
+$(".con>li>a>img").eq(0).on("click", function(){
+    $(".popupBg, .popup").show();
+});
+$("#close").on("click", function(){
+    $(".popupBg, .popup").hide();
+});
+
+$(".con>li>div>h2").eq(0).on("click", function(){
+    $(".popupBg, .popup").show();
+});
+$("#close").on("click", function(){
+    $(".popupBg, .popup").hide();
+});
+
+
+
+// 2번째
+$(".con>li>a>img").eq(1).on("click", function(){
+    $(".popupBg, .popup2").show();
+});
+$("#close2").on("click", function(){
+    $(".popupBg, .popup2").hide();
+});
+
+$(".con>li>div>h2").eq(1).on("click", function(){
+    $(".popupBg, .popup2").show();
+});
+$("#close2").on("click", function(){
+    $(".popupBg, .popup2").hide();
+});
+
+
+
+// 3번째
+$(".con>li>a>img").eq(2).on("click", function(){
+    $(".popupBg, .popup3").show();
+});
+$("#close3").on("click", function(){
+    $(".popupBg, .popup3").hide();
+});
+
+$(".con>li>div>h2").eq(2).on("click", function(){
+    $(".popupBg, .popup3").show();
+});
+$("#close3").on("click", function(){
+    $(".popupBg, .popup3").hide();
+});
+
+
+
+// 4번째
+$(".con>li>a>img").eq(3).on("click", function(){
+    $(".popupBg, .popup4").show();
+});
+$("#close4").on("click", function(){
+    $(".popupBg, .popup4").hide();
+});
+
+$(".con>li>div>h2").eq(3).on("click", function(){
+    $(".popupBg, .popup4").show();
+});
+$("#close4").on("click", function(){
+    $(".popupBg, .popup4").hide();
+});
+
+
+// 5번째
+$(".con>li>a>img").eq(4).on("click", function(){
+    $(".popupBg, .popup5").show();
+});
+$("#close5").on("click", function(){
+    $(".popupBg, .popup5").hide();
+});
+
+$(".con>li>div>h2").eq(4).on("click", function(){
+    $(".popupBg, .popup5").show();
+});
+$("#close5").on("click", function(){
+    $(".popupBg, .popup5").hide();
+});
+
+
+// 6번째
+$(".con>li>a>img").eq(5).on("click", function(){
+    $(".popupBg, .popup6").show();
+});
+$("#close6").on("click", function(){
+    $(".popupBg, .popup6").hide();
+});
+
+$(".con>li>div>h2").eq(5).on("click", function(){
+    $(".popupBg, .popup6").show();
+});
+$("#close6").on("click", function(){
+    $(".popupBg, .popup6").hide();
+});
+
+
+// 7번째
+$(".con>li>a>img").eq(6).on("click", function(){
+    $(".popupBg, .popup7").show();
+});
+$("#close7").on("click", function(){
+    $(".popupBg, .popup7").hide();
+});
+
+$(".con>li>div>h2").eq(6).on("click", function(){
+    $(".popupBg, .popup7").show();
+});
+$("#close7").on("click", function(){
+    $(".popupBg, .popup7").hide();
+});
+
+
+// 8번째
+$(".con>li>a>img").eq(7).on("click", function(){
+    $(".popupBg, .popup8").show();
+});
+$("#close8").on("click", function(){
+    $(".popupBg, .popup8").hide();
+});
+
+$(".con>li>div>h2").eq(7).on("click", function(){
+    $(".popupBg, .popup8").show();
+});
+$("#close8").on("click", function(){
+    $(".popupBg, .popup8").hide();
+});
